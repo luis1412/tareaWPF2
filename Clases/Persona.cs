599941f4995669555fb8fbce358e076b8b58bfba
luis1412 <luisherradorcruz@gmail.com>
2023-11-26 18:27:14 +0100

    TERMINADOOOO

--- Clases/Persona.cs
+++ Clases/Persona.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,50 +9,55 @@ namespace Clases2.Clases
 {
     public class Persona
     {
-        private string nombre;
 
-        public string rutaFoto { get; set; }
+        public string rutaFoto { get { return "profesores\\" + Nombre + ".png"; } set { } }
         public string Nombre { get; set; }
         public string apellidos { get; set; }
         public int edad { get; set; }
+
+        private string _id;
+
         public string Id { get; set; }
 
-       
+
+
         public Persona() { }
 
         public Persona(string nombre, string apellidos, int edad, string rutaFoto)
         {
-            this.nombre = nombre;
+            this.Nombre = nombre;
             this.apellidos = apellidos;
             this.edad = edad;
             this.Id = GenerarEmail();
             this.rutaFoto = "profesores\\" + rutaFoto;
         }
-       
+
+
+
         public virtual String GenerarEmail()
         {
-            string[] apellidos = this.apellidos.Split(' ');
-            string emailLocal="";
+            string[] apellidos = this.apellidos.Split(" ");
+            string emailLocal = "";
             if (apellidos.Length == 1)
             {
-                emailLocal = apellidos[0].Substring(0, 2).ToLower() + apellidos[0].Substring(0, 2).ToLower() + nombre.Substring(0, 1).ToLower();
+                emailLocal = apellidos[0].Substring(0, 2).ToLower() + apellidos[0].Substring(0, 2).ToLower() + Nombre.Substring(0, 1).ToLower();
             }
             else
             {
-                emailLocal = apellidos[0].Substring(0, 2).ToLower() + apellidos[1].Substring(0, 2).ToLower() + nombre.Substring(0, 1).ToLower();
+                emailLocal = apellidos[0].Substring(0, 2).ToLower() + apellidos[1].Substring(0, 2).ToLower() + Nombre.Substring(0, 1).ToLower();
             }
             return emailLocal + "@trass.com";
         }
 
         public override string ToString()
         {
-            return $"{nombre.PadRight(5)} \t {apellidos.PadRight(5)} \t {Id.PadRight(15)} \t {edad}";
+            return $"{Nombre.PadRight(5)} \t {apellidos.PadRight(5)} \t {Id.PadRight(15)} \t {edad}";
         }
 
         public override bool Equals(object? obj)
         {
             return obj is Persona persona &&
-                   nombre == persona.nombre &&
+                   Nombre == persona.Nombre &&
                    apellidos == persona.apellidos &&
                    edad == persona.edad;
         }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Clases2.Clases;
 using ejercicio8DI.Clases;
 using ejercicio8DI.Contextos;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using static Clases2.Clases.EmpleadoPublico;
@@ -26,8 +28,13 @@ namespace ejercicio8DI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
+
     public partial class MainWindow : Window
     {
+        public bool anadirOActualizar;
         List<ProfesorFuncionario> listaProfesores = new List<ProfesorFuncionario>();
         List<ProfesorExtendido> listaProfesoresExtendidos = new List<ProfesorExtendido>();
         int indiceActualProfesor = 0;
@@ -43,18 +50,22 @@ namespace ejercicio8DI
             menuFiltros.IsEnabled = false;
             menuAgrupacion.IsEnabled = false;
 
-          
+
             //Imagenes flechas
             imgPrimero.Source = (new ImageSourceConverter()).ConvertFromString(rutaFija + "flechaIzquierda.png") as ImageSource;
             imgSegundo.Source = (new ImageSourceConverter()).ConvertFromString(rutaFija + "flechaGordaIzquierda.png") as ImageSource;
             imgTercero.Source = (new ImageSourceConverter()).ConvertFromString(rutaFija + "flechaGordaDerecha.png") as ImageSource;
             imgCuarto.Source = (new ImageSourceConverter()).ConvertFromString(rutaFija + "flechaDerecha.png") as ImageSource;
-           //Imagenes interaccion usuarios y profesores
+            //Imagenes interaccion usuarios y profesores
             imgActualizarUsuario.Source = (new ImageSourceConverter()).ConvertFromString(rutaFija + "actualizarUsuario.png") as ImageSource;
             imgAnadir.Source = (new ImageSourceConverter()).ConvertFromString(rutaFija + "agregarUsuario.png") as ImageSource;
             imgBorrarUsuario.Source = (new ImageSourceConverter()).ConvertFromString(rutaFija + "borrarUsuario.png") as ImageSource;
             imgCancelar.Source = (new ImageSourceConverter()).ConvertFromString(rutaFija + "eliminar.png") as ImageSource;
             imgGuardar.Source = (new ImageSourceConverter()).ConvertFromString(rutaFija + "guardar.png") as ImageSource;
+
+
+
+            btnAnadirUsuario.IsEnabled = true;
         }
 
         private void menuArchivo_Click(object sender, RoutedEventArgs e)
@@ -62,7 +73,8 @@ namespace ejercicio8DI
 
         }
 
-        private void cargarDatos(ProfesorFuncionario profesorActual) {
+        private void cargarDatos(ProfesorFuncionario profesorActual)
+        {
             tbNombre.Text = profesorActual.Nombre;
             tbApellidos.Text = profesorActual.apellidos;
             tbEmail.Text = profesorActual.Id;
@@ -91,40 +103,46 @@ namespace ejercicio8DI
             {
                 seguroMedico.SelectedIndex = 0;
             }
-            else { 
+            else
+            {
                 seguroMedico.SelectedIndex = 1;
             }
             string rutaCompleta = "..\\..\\..\\imagenes\\" + profesorActual.rutaFoto;
-            imagenProfesor.Source = (new ImageSourceConverter()).ConvertFromString(rutaCompleta) as ImageSource;
+
+            // imagenProfesor.Source = (new ImageSourceConverter()).ConvertFromString(rutaCompleta) as ImageSource;
 
 
         }
 
 
 
-        public void anadirRegistrosLista() {
-            using (Contexto context = new Contexto()) {
-              listaProfesores = context.ProfesoresFuncionarios.ToList();
+        public void anadirRegistrosLista()
+        {
+            using (Contexto context = new Contexto())
+            {
+                listaProfesores = context.ProfesoresFuncionarios.ToList();
                 listaProfesoresExtendidos = context.ProfesoresExtendidos.ToList();
                 cargarDatos(listaProfesores[0]);
             }
-        
+
         }
 
 
 
-        public void activarCamposNuevo(bool activado) {
-            btnAnadirUsuario.IsEnabled = activado;
+        public void activarCamposNuevo(bool activado)
+        {
             btnActualizarUsuario.IsEnabled = activado;
-            btnAnadirUsuario.IsEnabled = activado;
-            btnCancelarUsuario.IsEnabled = activado;
+            btnPrimero.IsEnabled = activado;
+            btnSegundo.IsEnabled = activado;
+            btnTercero.IsEnabled = activado;
+            btnCuarto.IsEnabled = activado;
             btnBorrarUsuario.IsEnabled = activado;
-            btnGuardarUsuario.IsEnabled = activado;
 
         }
 
 
-        public void borrarCampos() {
+        public void borrarCampos()
+        {
             tbNombre.Text = "";
             tbApellidos.Text = "";
             tbEmail.Text = "";
@@ -134,7 +152,8 @@ namespace ejercicio8DI
         }
 
 
-        public void cargarCombo() {
+        public void cargarCombo()
+        {
             cbEdad.Items.Clear();
             for (int i = 22; i < 70; i++)
             {
@@ -144,67 +163,90 @@ namespace ejercicio8DI
 
         private void Nuevo_Click(object sender, RoutedEventArgs e)
         {
-            activarControles();
-            activarDesactivarCamposText(false);
-            borrarCampos();
-            activarCamposNuevo(true);
-            cargarCombo();
+
         }
 
-        public void activarDesactivarCamposText(bool isEnabled) {
+        public void activarDesactivarCamposText(bool isEnabled)
+        {
             tbNombre.IsReadOnly = isEnabled;
             tbApellidos.IsReadOnly = isEnabled;
-            tbEmail.IsReadOnly = false;
+            tbEmail.IsReadOnly = true;
             destino.IsEnabled = !isEnabled;
             cbEdad.IsReadOnly = isEnabled;
             tbIngreso.IsReadOnly = isEnabled;
             rbPracticas.IsEnabled = !isEnabled;
             rbCarrera.IsEnabled = !isEnabled;
             seguroMedico.IsEnabled = !isEnabled;
-            lblRutaFoto.Visibility = Visibility.Visible;
-            imagenProfesor.Visibility = Visibility.Hidden;
         }
 
 
-        public bool comprobarCampos() {
+        public bool comprobarCampos()
+        {
             int resultado;
 
-            if (tbNombre.Text.Length! > 0)
+            if (tbNombre.Text.Length <= 0)
             {
                 MessageBox.Show("El nombre no puede estar vacio");
+                tbNombre.Focus();
                 return false;
             }
-            else if (tbApellidos.Text.Length! > 0)
+            else if (tbApellidos.Text.Length <= 0)
             {
                 MessageBox.Show("El apellido no puede estar vacio");
+                tbApellidos.Focus();
                 return false;
             }
             else if (tbIngreso.Text.Length <= 0 || !Int32.TryParse(tbIngreso.Text, out resultado))
             {
+                tbIngreso.Focus();
                 MessageBox.Show("El año de ingreso no puede estar vacio, o no lo has introducido con numeros enteros");
                 return false;
             }
-            else {
+            else if (seguroMedico.SelectedIndex == -1)
+            {
+                seguroMedico.Focus();
+                MessageBox.Show("Selecciona un seguro medico");
+                return false;
+            }
+            else
+            {
                 return true;
             }
         }
 
-        private void Abrir_Click(object sender, RoutedEventArgs e)
-        {
-            activarCamposNuevo(false);
-            if (comprobarRegistroBD())
-            {
-                anadirRegistrosLista();
-                activarControles();
-            }
-            else {
-                seleccionarArchivoYCargar();
-            }
-            
+
+        public void activarFlechas(bool activado) {
+            btnPrimero.IsEnabled = activado;
+            btnSegundo.IsEnabled = activado;
+            btnTercero.IsEnabled = activado;
+            btnCuarto.IsEnabled = activado;
+            btnActualizarUsuario.IsEnabled = activado;
+            btnBorrarUsuario.IsEnabled = activado;
+            btnGuardarUsuario.IsEnabled = !activado;
+            btnCancelarUsuario.IsEnabled = !activado;
         }
 
 
-        public void seleccionarArchivoYCargar() {
+        private void Abrir_Click(object sender, RoutedEventArgs e)
+        {
+            activarFlechas(true);
+
+
+            if (comprobarRegistroBD())
+            {
+                anadirRegistrosLista();
+                activarControles(true);
+            }
+            else
+            {
+                seleccionarArchivoYCargar();
+            }
+
+        }
+
+
+        public void seleccionarArchivoYCargar()
+        {
             OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
 
             //Nos situamos en el directorio desde el que se ejecuta la aplicación
@@ -250,7 +292,7 @@ namespace ejercicio8DI
                         listaProfesores.Add(p);
                     }
                     cargarDatos(listaProfesores[0]);
-                    activarControles();
+                    activarControles(true);
                     anadirFicheroABD(listaProfesores);
                 }
                 catch (Exception ex)
@@ -260,14 +302,16 @@ namespace ejercicio8DI
             }
         }
 
-        public void activarControles() {
-            gridBotones.IsEnabled = true;
-            gridContenido.IsEnabled = true;
-            menuFiltros.IsEnabled = true;
-            menuAgrupacion.IsEnabled = true;
+        public void activarControles(bool activar)
+        {
+            gridBotones.IsEnabled = activar;
+            gridContenido.IsEnabled = activar;
+            menuFiltros.IsEnabled = activar;
+            menuAgrupacion.IsEnabled = activar;
         }
 
-        public void anadirFicheroABD(List<ProfesorFuncionario> profesorFuncionarios, List<ProfesorExtendido> profesorExtendidos = null) {
+        public void anadirFicheroABD(List<ProfesorFuncionario> profesorFuncionarios, List<ProfesorExtendido> profesorExtendidos = null)
+        {
             using (Contexto context = new Contexto())
             {
                 context.ProfesoresFuncionarios.AddRange(profesorFuncionarios);
@@ -282,7 +326,8 @@ namespace ejercicio8DI
         }
 
 
-        public bool comprobarRegistroBD() {
+        public bool comprobarRegistroBD()
+        {
             var context = new Contexto();
 
             if (!context.ProfesoresExtendidos.Any() && !context.ProfesoresFuncionarios.Any())
@@ -290,11 +335,12 @@ namespace ejercicio8DI
                 MessageBox.Show("No hay nigun registro en la base de datos");
                 return false;
             }
-            else {
+            else
+            {
                 return true;
             }
 
-             
+
         }
 
         private void btnPrimero_Click(object sender, RoutedEventArgs e)
@@ -315,12 +361,12 @@ namespace ejercicio8DI
 
         private void btnTercero_Click(object sender, RoutedEventArgs e)
         {
-            if (indiceActualProfesor < listaProfesores.Count-1)
+            if (indiceActualProfesor < listaProfesores.Count - 1)
             {
                 indiceActualProfesor++;
                 cargarDatos(listaProfesores[indiceActualProfesor]);
             }
-            
+
         }
 
         private void btnCuarto_Click(object sender, RoutedEventArgs e)
@@ -358,7 +404,7 @@ namespace ejercicio8DI
         {
             foreach (var control in gridContenido.Children)
             {
-                if (control is Control) 
+                if (control is Control)
                 {
                     Control elemento = (Control)control;
                     elemento.FontWeight = FontWeights.Bold;
@@ -409,7 +455,7 @@ namespace ejercicio8DI
 
         private void filtro1(object sender, RoutedEventArgs e)
         {
-            Auxiliar.mayores35(listaProfesores); 
+            Auxiliar.mayores35(listaProfesores);
         }
 
         private void filtro2(object sender, RoutedEventArgs e)
@@ -436,29 +482,166 @@ namespace ejercicio8DI
 
         private void btnAnadir_Click(object sender, RoutedEventArgs e)
         {
-
+            activarControles(true);
+            activarDesactivarCamposText(false);
+            borrarCampos();
+            activarFlechas(false);
+            cargarCombo();
+            anadirOActualizar = true;
         }
 
         private void btnActualizar_Click(object sender, RoutedEventArgs e)
         {
-
+            tbNombre.IsReadOnly = true;
+            tbApellidos.IsReadOnly = true;
+            tbEmail.IsReadOnly = true;
+            destino.IsEnabled = true;
+            cbEdad.IsReadOnly = false;
+            tbIngreso.IsReadOnly = false;
+            rbPracticas.IsEnabled = true;
+            rbCarrera.IsEnabled = true;
+            seguroMedico.IsEnabled = true;
+            btnGuardarUsuario.IsEnabled = true;
+            anadirOActualizar = false;
         }
 
         private void btnBorrar_Click(object sender, RoutedEventArgs e)
         {
 
+            MessageBoxResult resultado = MessageBox.Show("Quiere continuar", "Confirmacion", MessageBoxButton.OKCancel);
+
+            if (resultado == MessageBoxResult.OK)
+            {
+                ProfesorFuncionario p = listaProfesores[indiceActualProfesor];
+                using (Contexto context = new Contexto())
+                {
+
+                    context.Entry(p).State = EntityState.Deleted;
+                    context.SaveChanges();
+                    Abrir_Click(sender, new RoutedEventArgs(Button.ClickEvent));
+
+                }
+
+            }
+
+            }
+
+        public string GenerarEmail(string Nombre, string apellidos)
+        {
+            string[] apellidosArray = apellidos.Split(" ");
+            string emailLocal = "";
+            if (apellidosArray.Length == 1)
+            {
+                emailLocal = apellidosArray[0].Substring(0, 2).ToLower() + apellidosArray[0].Substring(0, 2).ToLower() + Nombre.Substring(0, 1).ToLower();
+            }
+            else
+            {
+                emailLocal = apellidosArray[0].Substring(0, 2).ToLower() + apellidosArray[1].Substring(0, 2).ToLower() + Nombre.Substring(0, 1).ToLower();
+            }
+            return emailLocal + "@trass.com";
         }
+
 
         private void btnCancelar_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Quiere continuar", "Confirmacion", MessageBoxButton.OKCancel);
+            MessageBoxResult resultado = MessageBox.Show("Quiere continuar", "Confirmacion", MessageBoxButton.OKCancel);
+
+
+
+            if (resultado == MessageBoxResult.OK)
+            {
+                if (comprobarRegistroBD())
+                {
+                    Abrir_Click(sender, new RoutedEventArgs(Button.ClickEvent));
+                    anadirRegistrosLista();
+                }
+                else
+                {
+                    activarCamposNuevo(false);
+                    activarControles(false);
+                }
+
+            }
+
         }
 
-        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+
+        public void guardarUsuarioNuevo() {
+            if (comprobarCampos())
+            {
+                ProfesorFuncionario p = new ProfesorFuncionario();
+                p.edad = Int32.Parse(cbEdad.SelectedItem.ToString());
+                p.apellidos = tbApellidos.Text;
+                if ((bool)rbCarrera.IsChecked)
+                {
+                    p.tipoFuncionario = TipoFuncionario.DeCarrera;
+
+                }
+                else if ((bool)rbPracticas.IsChecked)
+                {
+                    p.tipoFuncionario = TipoFuncionario.EnPracticas;
+
+                }
+                p.AnyoIngresoCuerpo = Int32.Parse(tbIngreso.Text);
+                p.DestinoDefinitivo = (bool)destino.IsChecked;
+                p.nombreImagen = tbNombre.Text + ".png";
+                p.tipoMedico = seguroMedico.SelectedIndex == 0 ? EmpleadoPublico.TipoMedico.SS : EmpleadoPublico.TipoMedico.MUFACE;
+                p.Nombre = tbNombre.Text;
+                p.Id = GenerarEmail(p.Nombre, p.apellidos);
+                p.Materia = "Sin Materia";
+                using (Contexto context = new Contexto())
+                {
+                    context.ProfesoresFuncionarios.Add(p);
+                    context.SaveChanges();
+                }
+
+                listaProfesores.Add(p);
+            }
+
+        }
+
+        public void actualizarUsuario()
         {
+           ProfesorFuncionario p = listaProfesores[indiceActualProfesor];
+
+            using (Contexto context = new Contexto())
+            {
+
+                if ((bool)rbCarrera.IsChecked)
+                {
+                    p.tipoFuncionario = TipoFuncionario.DeCarrera;
+
+                }
+                else if ((bool)rbPracticas.IsChecked)
+                {
+                    p.tipoFuncionario = TipoFuncionario.EnPracticas;
+
+                }
+                p.AnyoIngresoCuerpo = Int32.Parse(tbIngreso.Text);
+                p.DestinoDefinitivo = (bool)destino.IsChecked;
+                p.nombreImagen = tbNombre.Text + ".png";
+                p.tipoMedico = seguroMedico.SelectedIndex == 0 ? EmpleadoPublico.TipoMedico.SS : EmpleadoPublico.TipoMedico.MUFACE;
+                p.Nombre = tbNombre.Text;
+                p.Id = GenerarEmail(p.Nombre, p.apellidos);
+                p.Materia = "Sin Materia";
+
+                context.ProfesoresFuncionarios.Update(p);
+                    context.SaveChanges();
+                }
 
         }
 
- 
-    }
+
+            private void btnGuardar_Click(object sender, RoutedEventArgs e)
+            {
+                if (anadirOActualizar)
+                {
+                    guardarUsuarioNuevo();
+                }
+                else {
+                    actualizarUsuario();
+                }
+            }
+
+        }
 }
